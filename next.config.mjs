9cbedac7453e485bf91dc791a0900683f5f0ce93
mac <mac@hydrogen-as-always.local>
2026-05-14 02:57:07 +0800

    Fix: Remove headers from next.config - use vercel.json instead

--- next.config.mjs
+++ next.config.mjs
@@ -20,25 +20,6 @@ const nextConfig = {
   },
   compress: true,
   poweredByHeader: false,
-  headers: async () => [
-    {
-      source: '/:path*',
-      headers: [
-        {
-          key: 'X-DNS-Prefetch-Control',
-          value: 'on',
-        },
-        {
-          key: 'X-Frame-Options',
-          value: 'SAMEORIGIN',
-        },
-        {
-          key: 'X-Content-Type-Options',
-          value: 'nosniff',
-        },
-      ],
-    },
-  ],
 }
 
 export default nextConfig
